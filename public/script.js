505ce4b4eff88ae7a4c297d07e3a537a1bf85f91
Murilo Silva <murilo.silva@strv.com>
2021-06-23 18:31:06 -0300

    script organization

--- public/script.js
+++ public/script.js
@@ -12,42 +12,48 @@ let currentFilterIndex = 0;
 videolElement.muted = true
 videolElement.classList.add('me')
 
-videolElement.addEventListener('click', () => {
-  currentFilterIndex = currentFilterIndex < filters.length - 1 ? currentFilterIndex + 1 : 0
-  const currentFilter = filters[currentFilterIndex];
-  console.log('currentFilter', currentFilter)
-  videolElement.style.filter = currentFilter
-})
+handleFilters(videolElement)
 
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  addUserVideoStream(videolElement, stream)
-
-  mainPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-
-    call.on('stream', userVideoStrem => {
-      addUserVideoStream(video, userVideoStrem)
+function initApp() {
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+    addUserVideoStream(videolElement, stream)
+  
+    mainPeer.on('call', call => {
+      call.answer(stream)
+      const video = document.createElement('video')
+  
+      call.on('stream', userVideoStrem => {
+        addUserVideoStream(video, userVideoStrem)
+      })
+    })
+  
+    socket.on('user-connected', userId => {
+      console.log('user connected', userId)
+      makeUserConnection(userId, stream)
     })
   })
-
-  socket.on('user-connected', userId => {
-    console.log('user connected', userId)
-    makeUserConnection(userId, stream)
+  
+  socket.on('user-disconnected', userId => {
+    console.log('user disconnected', userId)
+    peers[userId]?.close()
   })
-})
+  
+  mainPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+  })
+}
 
-socket.on('user-disconnected', userId => {
-  console.log('user disconnected', userId)
-  peers[userId]?.close()
-})
-
-mainPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
+function handleFilters(video) {
+  video.addEventListener('click', () => {
+    currentFilterIndex = currentFilterIndex < filters.length - 1 ? currentFilterIndex + 1 : 0
+    const currentFilter = filters[currentFilterIndex];
+    console.log('currentFilter', currentFilter)
+    video.style.filter = currentFilter
+  })
+}
 
 function addUserVideoStream(video, stream) {
   video.srcObject = stream
@@ -73,3 +79,5 @@ function makeUserConnection(userId, stream) {
 
   peers[userId] = call
 }
+
+initApp()
